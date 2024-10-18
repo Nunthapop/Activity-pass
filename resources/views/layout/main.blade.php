97ec80b8dc757011cabebb48c1c61bb1db4ce32a
Arthur091 <127417960+Nunthapop@users.noreply.github.com>
2024-10-18 18:16:43 +0700

    Controller set up

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Activity-pass</title>
+    <link rel="stylesheet" href="resources/css/main.css">
 </head>
 <body>
-    
+    <nav>
+
+    </nav>
+
+    @yield('content')
 </body>
 </html>
