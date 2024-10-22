94da07a0e0b3bd9ce7e632964233df45358c7b59
Arthur091 <127417960+Nunthapop@users.noreply.github.com>
2024-10-22 18:01:05 +0700

    Searchable controller

---
+++ app/Http/Controllers/SearchableController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Psr\Http\Message\ServerRequestInterface;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+
+class SearchableController extends Controller
+{
+    
+}
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,10 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Psr\Http\Message\ServerRequestInterface;
+use Illuminate\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Http\RedirectResponse; 
+use App\Models;
 use Illuminate\Http\Request;
+use App\Models\Student;
+
 
 class StudentController extends Controller
 {
-    //
+    public function getQuery(): Builder
+    {
+        return Student::orderby('name');
+    }
+    function list(ServerRequestInterface $request): View
+    {
+        // $students = $this->getQuery()->paginate(10);
+
+        return view('student.list',[
+
+        ]);
+    }
 }
