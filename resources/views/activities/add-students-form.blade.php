9b30f7099764b45a7f1669b3f6bf3b2315a406df
HKonlawat <160825310+HKonlawat@users.noreply.github.com>
2024-10-29 02:28:35 +0700

    formaddac

--- resources/views/activities/add-students-form.blade.php
+++ resources/views/activities/add-students-form.blade.php
@@ -14,49 +14,51 @@
         <a href="{{ route('activities.add-students-form', ['activity_name' => $activity->name]) }}">
             <button type="button" class="accent">Clear</button>
         </a>
+
+        @csrf
+        <main>
+            <!-- ปุ่ม action -->
+            <nav>
+                <li>
+                    <a href="{{ route('activities.view-students', ['activity_name' => $activity->name]) }}">
+                        <button type="button" class="back-button">Back</button>
+                    </a>
+                </li>
+                </ul>
+            </nav>
+
+            <!-- แสดงหน้าลิงค์เพจ -->
+            <div>{{ $students->withQueryString()->links() }}</div>
+
+            <table class="lg:w-1/2">
+                <tr>
+                    <th>Code</th>
+                    <th>Name</th>
+                    <th>Year</th>
+                    <th>Major</th>
+                    <th>Score</th>
+                    <th>Add</th>
+                </tr>
+                <tbody>
+                    @foreach ($students as $item)
+                        <tr>
+                            <td class="underline">
+                                {{ $item->code }} </td>
+                            <td> {{ $item->first_name }} {{ $item->last_name }}</td>
+                            <td>{{ $item->year }}</td>
+                            <td>{{ $item->major }}</td>
+                            <td>{{ $item->score }}</td>
+                            <td> <button type="submit" name="code" value="{{ $item->code }}"
+                                    class="add-button">Add</button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+        </main>
+
     </form>
-
-    <main>
-        <!-- ปุ่ม action -->
-        <nav>
-            <li>
-                <a href="{{ route('activities.view-students', ['activity_name' => $activity->name]) }}">
-                    <button type="button" class="back-button">Back</button>
-                </a>
-            </li>
-            </ul>
-        </nav>
-
-        <!-- แสดงหน้าลิงค์เพจ -->
-        <div>{{ $students->withQueryString()->links() }}</div>
-
-        <table class="lg:w-1/2">
-            <tr>
-                <th>Code</th>
-                <th>Name</th>
-                <th>Year</th>
-                <th>Major</th>
-                <th>Score</th>
-                <th>Add</th>
-            </tr>
-            <tbody>
-                @foreach ($students as $item)
-                    <tr>
-                        <td class="underline">
-                            {{ $item->code }} </td>
-                        <td> {{ $item->first_name }} {{ $item->last_name }}</td>
-                        <td>{{ $item->year }}</td>
-                        <td>{{ $item->major }}</td>
-                        <td>{{ $item->score }}</td>
-                        <td> <button type="submit" name="code" value="{{ $item->code }}"
-                                class="add-button">Add</button>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-    </main>
 
     </html>
 
