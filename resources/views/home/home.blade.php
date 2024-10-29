a719e4a95e884ae56b6f5258f65721a0ec861fea
ecilanit04 <ttiinnaaww47@gmail.com>
2024-10-30 01:18:13 +0700

    home edit

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,7 +5,7 @@
     <body>
     <link rel="stylesheet" href="{{ asset('css/publichome.css') }}" type="text/css">
     <img src="{{ asset('images/home.png') }}" aly="bg">
-    <a href="{{ route('login') }}">Click Here To Login</a>
+    <a href="{{ route('login') }}">Login</a>
     </body>
 </html>
 
